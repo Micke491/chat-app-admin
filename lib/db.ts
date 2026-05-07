4c4d0a2906f11527748727da137f0fa67205a186
Micke491 <nikolamicic07@gmail.com>
2026-05-07 18:04:00 +0200

    changed db

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,7 @@ import "@/models/Story";
 import "@/models/Report";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
+const DB_NAME = process.env.DB_NAME || "chat-app"; 
 
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI in .env.local");
@@ -20,6 +21,7 @@ export async function connectDB() {
     cached.promise = mongoose
       .connect(MONGODB_URI, {
         bufferCommands: false,
+        dbName: DB_NAME, 
       })
       .then((mongoose) => mongoose);
     (global as any).mongoose = cached;
